75fca9837f7af2fb111cb343647860aba6697f96
piilzner <nils.lofgren@hotmail.com>
2015-12-14 23:30:44 +0100

    fixed article section

--- www/js/ctrl/articlesCtrl.js
+++ www/js/ctrl/articlesCtrl.js
@@ -1,4 +1,4 @@
-app.controller('articlesCtrl', ["$scope", function($scope){
+app.controller('articlesCtrl', ["$scope", "$http", function($scope, $http){
     
     
       
@@ -7,6 +7,39 @@ $('.box-wrapper').masonry({
   itemSelector: '.box'
 });
     
+    
+    
+  $scope.articles = [
+      {
+          title : "Piilzners boilerplate",
+          desc : "Piilzners boilerplate is a Angular.JS filestructure including, gulp, Angular.JS, jQuery, Bootstrap and font-awesome",
+          image : "www/img/pbp.jpg",
+          link : "https://github.com/piilzner/Boilerplate-for-AngularJS"
+          
+      },
+      {
+          title : "happnings",
+          desc : "happnings is a webb / mobile application that lists event on a central place",
+          image : "www/img/happnings.jpg",
+          link : "http://happnings.se/#/"
+          
+      },
+      {
+          title : "DotList",
+          desc : "DotList is a mobile app build with the phonegap and cordova framework. You can make lists with tasks or items on it. You can also invite others to youre list.",
+          image : "www/img/dotlist.png"
+          
+      },
+      {
+          title : "Ari´s Gold",
+          desc : "As a fun sideproject i wanted to make a beer label design and this is the result.",
+          image : "www/img/beer.jpg"
+          
+      }
+      
+  ];
+    
+    console.log($scope.articles);
 
     
 }]);
--- www/src/main.js
+++ www/src/main.js
@@ -22,15 +22,6 @@ app.config(function($locationProvider, $routeProvider) {
     }).otherwise({ redirectTo: '/' });
 });
 
-app.directive('menubar', function() {
-  return {
-    restrict: 'E',
-    controller: ['$scope', function($scope){
-        
-    }],
-    templateUrl: 'www/views/partial/menubar.html'
-  };
-});
 app.controller('aboutCtrl', ["$scope", function($scope){
     
     
@@ -39,7 +30,7 @@ app.controller('aboutCtrl', ["$scope", function($scope){
 
     
 }]);
-app.controller('articlesCtrl', ["$scope", function($scope){
+app.controller('articlesCtrl', ["$scope", "$http", function($scope, $http){
     
     
       
@@ -48,6 +39,39 @@ $('.box-wrapper').masonry({
   itemSelector: '.box'
 });
     
+    
+    
+  $scope.articles = [
+      {
+          title : "Piilzners boilerplate",
+          desc : "Piilzners boilerplate is a Angular.JS filestructure including, gulp, Angular.JS, jQuery, Bootstrap and font-awesome",
+          image : "www/img/pbp.jpg",
+          link : "https://github.com/piilzner/Boilerplate-for-AngularJS"
+          
+      },
+      {
+          title : "happnings",
+          desc : "happnings is a webb / mobile application that lists event on a central place",
+          image : "www/img/happnings.jpg",
+          link : "http://happnings.se/#/"
+          
+      },
+      {
+          title : "DotList",
+          desc : "DotList is a mobile app build with the phonegap and cordova framework. You can make lists with tasks or items on it. You can also invite others to youre list.",
+          image : "www/img/dotlist.png"
+          
+      },
+      {
+          title : "Ari´s Gold",
+          desc : "As a fun sideproject i wanted to make a beer label design and this is the result.",
+          image : "www/img/beer.jpg"
+          
+      }
+      
+  ];
+    
+    console.log($scope.articles);
 
     
 }]);
@@ -110,3 +134,12 @@ app.controller('projectsCtrl', ["$scope", "$http", function($scope, $http){
     console.log($scope.projects);
     
 }]);
+app.directive('menubar', function() {
+  return {
+    restrict: 'E',
+    controller: ['$scope', function($scope){
+        
+    }],
+    templateUrl: 'www/views/partial/menubar.html'
+  };
+});
