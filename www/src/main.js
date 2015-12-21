b5019f77897809eb5ae368fa445f6ad67405715a
piilzner <nils.lofgren@hotmail.com>
2015-12-21 21:07:21 +0100

    anchor scroll fix

--- www/src/main.js
+++ www/src/main.js
@@ -33,6 +33,53 @@ app.config(function($locationProvider, $routeProvider) {
     }).otherwise({ redirectTo: '/404' });
 });
 
+app.directive('footer', function() {
+  return {
+    restrict: 'E',
+    controller: ['$scope', function($scope){
+        
+      
+           
+        
+    }],
+    templateUrl: 'www/views/partial/footer.html'
+  };
+    
+
+    
+    
+});
+app.directive('menubar', function() {
+  return {
+    restrict: 'E',
+    controller: ['$scope', function($scope){
+        
+        $('.menu-ham').click(function(){
+            
+            if($('.menu').css('right') == '0px'){
+                $('.menu').css('right', '-500px');
+                $('.ham-one').css({'background' : '#17a19f', 'transform' : 'rotate(0deg)', 'margin-top' : '0', 'width' : '100%'});
+                $('.ham-three').css({'background' : '#17a19f', 'transform' : 'rotate(0deg)', 'margin-top' : '0', 'width' : '100%'});
+                $('.ham-two').css('display', 'block');
+            }else{
+                $('.menu').css('right', '0px');
+                 $('.ham-one').css({'background' : '#2b2b2b', 'transform' : 'rotate(-45deg)', 'margin-top' : '27px', 'width' : '70%'});
+                $('.ham-two').css('display', 'none');
+                $('.ham-three').css({'background' : '#2b2b2b', 'transform' : 'rotate(45deg)', 'margin-top' : '-27px', 'width' : '70%'});
+                
+            }
+        });
+        
+           
+        
+    }],
+    templateUrl: 'www/views/partial/menubar.html'
+  };
+    
+
+    
+    
+});
 app.controller('aboutCtrl', ["$scope", function($scope){
     $('body').scrollTop(0,0);
     
@@ -137,20 +184,11 @@ app.controller('homeCtrl', ["$scope", "projects", function($scope, projects){
     
     console.log($scope.projects);
     
-    
-    $('a[href^="#"]').on('click',function (e) {
-	    e.preventDefault();
-
-	    var target = this.hash;
-	    var $target = $(target);
-
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 900, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
-    
+$("#scroll-down").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#simple-about").offset().top
+    }, 2000);
+});
     
     
 }]);
@@ -194,53 +232,6 @@ app.controller('projectsCtrl', ["$scope", "$http", "projects", function($scope, 
     console.log($scope.projects);
     
 }]);
-app.directive('footer', function() {
-  return {
-    restrict: 'E',
-    controller: ['$scope', function($scope){
-        
-      
-           
-        
-    }],
-    templateUrl: 'www/views/partial/footer.html'
-  };
-    
-
-    
-    
-});
-app.directive('menubar', function() {
-  return {
-    restrict: 'E',
-    controller: ['$scope', function($scope){
-        
-        $('.menu-ham').click(function(){
-            
-            if($('.menu').css('right') == '0px'){
-                $('.menu').css('right', '-500px');
-                $('.ham-one').css({'background' : '#17a19f', 'transform' : 'rotate(0deg)', 'margin-top' : '0', 'width' : '100%'});
-                $('.ham-three').css({'background' : '#17a19f', 'transform' : 'rotate(0deg)', 'margin-top' : '0', 'width' : '100%'});
-                $('.ham-two').css('display', 'block');
-            }else{
-                $('.menu').css('right', '0px');
-                 $('.ham-one').css({'background' : '#2b2b2b', 'transform' : 'rotate(-45deg)', 'margin-top' : '27px', 'width' : '70%'});
-                $('.ham-two').css('display', 'none');
-                $('.ham-three').css({'background' : '#2b2b2b', 'transform' : 'rotate(45deg)', 'margin-top' : '-27px', 'width' : '70%'});
-                
-            }
-        });
-        
-           
-        
-    }],
-    templateUrl: 'www/views/partial/menubar.html'
-  };
-    
-
-    
-    
-});
 app.factory('articles', function() {
 	
     
@@ -249,7 +240,7 @@ app.factory('articles', function() {
           title : "Piilzners boilerplate",
           desc : "Piilzners boilerplate är en Angular.JS filstruktur som inkluderar gulp, Angular.JS, jQuery, Bootstrap och Font-awesome. Ett smidigt sätt när man startar ett nytt projekt. Det är bara clona projektet från github och man är igång. ",
           image : "www/img/pbp.jpg",
-          date : "12-12-2015",
+          date : "27-11-2015",
           author : "Nils Löfgren",
           site : "https://github.com/piilzner/Boilerplate-for-AngularJS",
           genre : [
@@ -261,7 +252,7 @@ app.factory('articles', function() {
           title : "Ari´s Gold",
           desc : "Ari´s Gold är resultatet av ett kul sidoprojekt. Tanken var att göra en rolig öl-etikett design och jag valde Ari Gold från 'Entourage'. Hade denna öl funnits i butik hade jag inte tvekat 1 sekund att köpa den.  ",
           image : "www/img/beer.jpg",
-          date : "12-12-2015",
+          date : "13-10-2015",
           author : "Nils Löfgren",
           site : "",
           genre : [
