fb6edf83b27691cacacf49c82be6b7c43bb8782a
piilzner <nils.lofgren@hotmail.com>
2015-12-21 11:43:05 +0100

    fixed spelling and grammar

--- www/src/main.js
+++ www/src/main.js
@@ -33,53 +33,6 @@ app.config(function($locationProvider, $routeProvider) {
     }).otherwise({ redirectTo: '/404' });
 });
 
-app.directive('footer', function() {
-  return {
-    restrict: 'E',
-    controller: ['$scope', function($scope){
-        
-      
-           
-        
-    }],
-    templateUrl: 'www/views/partial/footer.html'
-  };
-    
-
-    
-    
-});
-app.directive('menubar', function() {
-  return {
-    restrict: 'E',
-    controller: ['$scope', function($scope){
-        
-        $('.menu-ham').click(function(){
-            
-            if($('.menu').css('right') == '0px'){
-                $('.menu').css('right', '-500px');
-                $('.ham-one').css({'background' : '#17a19f', 'transform' : 'rotate(0deg)', 'margin-top' : '0', 'width' : '100%'});
-                $('.ham-three').css({'background' : '#17a19f', 'transform' : 'rotate(0deg)', 'margin-top' : '0', 'width' : '100%'});
-                $('.ham-two').css('display', 'block');
-            }else{
-                $('.menu').css('right', '0px');
-                 $('.ham-one').css({'background' : '#2b2b2b', 'transform' : 'rotate(-45deg)', 'margin-top' : '27px', 'width' : '70%'});
-                $('.ham-two').css('display', 'none');
-                $('.ham-three').css({'background' : '#2b2b2b', 'transform' : 'rotate(45deg)', 'margin-top' : '-27px', 'width' : '70%'});
-                
-            }
-        });
-        
-           
-        
-    }],
-    templateUrl: 'www/views/partial/menubar.html'
-  };
-    
-
-    
-    
-});
 app.controller('aboutCtrl', ["$scope", function($scope){
     $('body').scrollTop(0,0);
     
@@ -226,6 +179,53 @@ app.controller('projectsCtrl', ["$scope", "$http", "projects", function($scope, 
     console.log($scope.projects);
     
 }]);
+app.directive('footer', function() {
+  return {
+    restrict: 'E',
+    controller: ['$scope', function($scope){
+        
+      
+           
+        
+    }],
+    templateUrl: 'www/views/partial/footer.html'
+  };
+    
+
+    
+    
+});
+app.directive('menubar', function() {
+  return {
+    restrict: 'E',
+    controller: ['$scope', function($scope){
+        
+        $('.menu-ham').click(function(){
+            
+            if($('.menu').css('right') == '0px'){
+                $('.menu').css('right', '-500px');
+                $('.ham-one').css({'background' : '#17a19f', 'transform' : 'rotate(0deg)', 'margin-top' : '0', 'width' : '100%'});
+                $('.ham-three').css({'background' : '#17a19f', 'transform' : 'rotate(0deg)', 'margin-top' : '0', 'width' : '100%'});
+                $('.ham-two').css('display', 'block');
+            }else{
+                $('.menu').css('right', '0px');
+                 $('.ham-one').css({'background' : '#2b2b2b', 'transform' : 'rotate(-45deg)', 'margin-top' : '27px', 'width' : '70%'});
+                $('.ham-two').css('display', 'none');
+                $('.ham-three').css({'background' : '#2b2b2b', 'transform' : 'rotate(45deg)', 'margin-top' : '-27px', 'width' : '70%'});
+                
+            }
+        });
+        
+           
+        
+    }],
+    templateUrl: 'www/views/partial/menubar.html'
+  };
+    
+
+    
+    
+});
 app.factory('articles', function() {
 	
     
