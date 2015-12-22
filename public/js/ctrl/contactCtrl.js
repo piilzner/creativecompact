f33085b4d2b816aeab2e12c8a8f6940fe6a0c87f
Filip Ramstedt <magra@magra.lan>
2015-12-22 15:30:51 +0100

    recompiled main.js

--- public/js/ctrl/contactCtrl.js
+++ public/js/ctrl/contactCtrl.js
@@ -2,18 +2,18 @@ app.controller('contactCtrl', ["$scope", "$http", function($scope, $http){
     $('body').scrollTop(0,0);
     
     $scope.sendMessage = function() {
-        
         $http.post("/message", {
             email: $scope.email,
             name: $scope.name,
             message: $scope.message
         }).then(function(data){
-            console.log(data.data);
             if(data.data.error == "novalid"){
                 $scope.errormsg = data.data.message;
             }else{
+                $scope.email = "";
+                $scope.name = "";
+                $scope.message = "";
                 $('.message-sucess').css('right', "0");
-        
                 setTimeout(function(){
                     $('.message-sucess').css('right', '-320px');
                 }, 3000);
